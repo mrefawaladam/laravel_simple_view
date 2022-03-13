18d039a1cdc9b25631a8874784278c9738ce5ec5
mrefawaladam <aerfa55@gmail.com>
2022-03-13 22:07:21 +0700

    make faker & seder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'slug' => $this->faker->slug(),
+            'price'    => $this->faker->randomDigit(),
+            'file_image'     => $this->faker->imageUrl($width = 200, $height = 200),
+            'description' => $this->faker->paragraph
+        ];
+    }
+}
